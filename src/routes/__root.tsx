729ac5427f16b5fa61f9e167095b25f2fc506320
GeordiD <geordi.dosher@gmail.com>
2026-03-30 20:57:53 -0500

    fix format

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,6 @@
+import { TanStackDevtools } from '@tanstack/react-devtools';
 import { HeadContent, Scripts, createRootRouteWithContext } from '@tanstack/react-router';
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools';
-import { TanStackDevtools } from '@tanstack/react-devtools';
 import NavBar from '../components/NavBar';
 
 import TanStackQueryProvider from '../integrations/tanstack-query/root-provider';
