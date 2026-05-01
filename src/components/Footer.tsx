10e555e302050190a0dcc8b8a99ed0d4c87f80e1
GeordiD <geordi.dosher@gmail.com>
2026-04-30 22:12:36 -0500

    switch to pwa links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,11 @@
-import { useLocation } from '@tanstack/react-router';
+import { Link, useLocation } from '@tanstack/react-router';
 import { BookOpen, CalendarDays, PlusCircle, ShoppingCart } from 'lucide-react';
 
 export function Footer() {
   return (
     <>
       <nav className="fixed left-4 right-4 bottom-4 z-50 h-14">
-        <div className="flex items-center justify-between gap-2 h-full py-2 rounded-full border-2 border-border bg-card max-w-120 mx-auto">
+        <div className="flex items-center justify-between gap-2 h-full px-1 py-2 rounded-full border-2 border-border bg-card max-w-120 mx-auto">
           <Option icon={<CalendarDays size={20} />} url="/" label="Meal Plan" />
           <Option icon={<BookOpen size={20} />} url="/recipes" label="Recipe List" />
           <Option icon={<PlusCircle size={20} />} url="/add" label="Add Recipe" />
@@ -20,12 +20,12 @@ function Option({ icon, url, label }: { icon: React.ReactElement; url: string; l
   const location = useLocation();
   const isActive = location.pathname == url;
 
-  const defaultClasses = 'rounded-full p-4 transition hover:bg-accent hover:text-foreground';
+  const defaultClasses = 'rounded-full p-3 transition hover:bg-accent hover:text-foreground';
   const activeClasses = isActive ? 'bg-primary text-foreground' : '';
 
   return (
-    <a href={url} aria-label={label} className={[defaultClasses, activeClasses].join(' ')}>
+    <Link to={url} aria-label={label} className={[defaultClasses, activeClasses].join(' ')}>
       {icon}
-    </a>
+    </Link>
   );
 }
